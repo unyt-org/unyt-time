cc85c36046b8b0eda8c1f3b77650135ad37fe295
Marvin Klein <MarvinsMC@yahoo.de>
2025-04-07 12:06:25 +0200

    initial-commit

---
+++ backend/entrypoint.tsx
@@ -0,0 +1,15 @@
+/**
+ * Backend entrypoint:
+ * This module provides a default export that defines the UI that is returned from the backend
+ * when a page is visited
+ */
+import { provideRedirect } from "uix/providers/common.tsx";
+import { type Entrypoint } from "uix/providers/entrypoints.ts";
+
+export default {
+  // show backend (hybrid) rendered page on /backend
+  "/backend": import("common/page.tsx"),
+
+  // redirect / to /backend
+  "/": provideRedirect("/backend"),
+} satisfies Entrypoint;
